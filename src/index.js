2f3df73e99db5c8e230e2c59522bccbc3cc623fe
onion <onionscream@gmail.com>
2025-01-22 15:15:33 +0200

    a bit changed functionality2

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const hostname = '127.0.0.1';
 const app = express();
 const port = 3000;
 
-app.use('/', express.static('public'));
+app.use('/', express.static('src/public'));
 app.use(express.json());
 
 app.get('/', (req, res) => {
